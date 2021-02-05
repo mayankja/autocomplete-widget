b3e50949214fc08397b8c95ef1d8ecadbe391245
Mayank Jadam <mayankjadam74@gmail.com>
2021-02-05 17:04:18 +0530

    autocomplete-widget

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import './App.css';
+
+const listOptions = ['Banana', 'Apple', 'Avocado', 'Pineapple', 'Papaya', 'Orange', 'Peach']
+
+const App = () => {
+
+  const [list, setList] = useState(listOptions)
+
+  const filterOptions = (e) => {
+    if(e.target.value.length === 0) {
+      setList(listOptions)
+    } else {
+      let textInput = e.target.value.toLowerCase()
+      let filteredList = listOptions.filter(el => el.toLowerCase().startsWith(textInput))
+      setList(filteredList);
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="AutoCompleteWrapper">
+        <label>Auto Complete Widget:</label>
+        <input 
+          type="text" 
+          list="fruits" 
+          onChange={filterOptions}
+        />
+        <datalist id="fruits">
+          {list.map((el,i) => <option key={i}>{el}</option> )}
+        </datalist>
+      </div>
+    </div>
+  );
+}
+export default App;
